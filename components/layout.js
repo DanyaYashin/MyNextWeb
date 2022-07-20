434ac32a2ae4388abb2de3f9cced6c9acbd10f1b
DanyaYashin <48764147+DanyaYashin@users.noreply.github.com>
2022-07-20 18:20:16 +0400

    add circle menu

--- components/layout.js
+++ components/layout.js
@@ -1,73 +1,47 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
+import React, { useState, useEffect } from "react"
 
 const name = 'MyNextWeb';
-export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
+	const [menuActive, setMenuActive] = React.useState(false)
+	const [menuDisc1, setMenuDisc1] = React.useState(styles.disc1)
+	const [menuDisc2, setMenuDisc2] = React.useState(styles.disc2)
+	const toggleMenu = () => {
+    setMenuActive(!menuActive);
+    if (menuActive){
+      setMenuDisc1(styles.disc1)
+			setMenuDisc2(styles.disc2)
+    }
+    else {
+      setMenuDisc1(styles.disc11)
+			setMenuDisc2(styles.disc22)
+    }
+  }
+	const closeMenu = () =>{
+	 setMenuDisc1(styles.disc1)
+	 setMenuDisc2(styles.disc2)
+ }
   return (
     <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <Image
-                  priority
-                  src="/images/profile.jpg"
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt={name}
-                />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
+	       <a className={styles.disc4}>
+		       <div>Messages</div>
+	       </a>
+	       <a className={styles.disc3}>
+		       <div>Photos</div>
+         </a>
+	       <a className={menuDisc2}>
+		       <div>Profile</div>
+	       </a>
+	       <a className={menuDisc1}>
+		       <div>Likes</div>
+	       </a>
+	       <a className={styles.menu} onClick={toggleMenu}>
+		       Menu
+	       </a>
       <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-      )}
     </div>
   );
 }
